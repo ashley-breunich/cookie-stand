e0f259ec0bc4f5e9070fd6da09ee5fd6cc884bdd
Ashley Breunich <ashleybreunich@gmail.com>
2018-05-25 21:44:50 -0700

    Got new store to populate to page - looping through all previous locations though

--- app.js
+++ app.js
@@ -3,6 +3,10 @@
 var hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 
 var allLocations = [];
+
+var cookieStands = document.getElementById('cookiestands');
+var storeForm = document.getElementById('store-form');
+
 // var totalCookiesByHour = 0;
 // var totalDailySales = 0;
 
@@ -42,10 +46,10 @@ function makeStands() {
 makeStands();
 
 function header(){
-  var cookiestands = document.getElementById('cookiestands'); // find cookiestands ID
+  cookieStands; //find cookiestands ID
   var theadEl = document.createElement('thead'); // create head element called theadEl
   var thEl = document.createElement('th'); // create rowheader element called thEl
-  cookiestands.appendChild(theadEl); //links the two up - head to table
+  cookieStands.appendChild(theadEl); //links the two up - head to table
   theadEl.appendChild(thEl); // link the two up - header to head
   for (var i = 0; i < hoursOfOperation.length; i++){ // for as long as hours array
     thEl = document.createElement('th'); // create a header element
@@ -61,9 +65,9 @@ header ();
 
 MakeShopLocation.prototype.renderRow = function(){
   this.cookiesByHour();
-  var cookiestands = document.getElementById('cookiestands'); // find cookiestand ID
+  cookieStands; // find cookiestand ID
   var trEl = document.createElement('tr'); // create a row called trEl
-  cookiestands.appendChild(trEl);
+  cookieStands.appendChild(trEl);
   var thEl = document.createElement('th'); // create a header cell called thEl
   thEl.textContent = this.name; // Add the name of the store to that cell
   trEl.appendChild(thEl); //link the two up - header cell to row
@@ -84,13 +88,46 @@ function renderLocationRows() {
 }
 renderLocationRows();
 
+//Creating footer row
 function footer(){
   //totalTotal = 0;
-  var cookiestands = document.getElementById('cookiestands');
+  cookieStands;
   var tfootEl = document.createElement('tfoot');
-  cookiestands.appendChild(tfootEl);
+  cookieStands.appendChild(tfootEl);
   var thEl = document.createElement('th');
   thEl.textcontent = 'Daily Total';
   tfootEl.appendChild(thEl);
 }
 footer();
+
+//beginning of form functionality
+
+//function to clear fields
+function clearFields(event){
+  event.target.storename.value = null;
+  event.target.mincustomer.value = null;
+  event.target.maxcustomer.value = null;
+  event.target.avgcookie.value = null;
+}
+
+//REMINDER(DECLARED UP TOP) - var storeForm = document.getElementById('store-form');
+storeForm;
+function handleAdditionalStore(event) {
+  event.preventDefault();
+  if (!event.target.storename.value || !event.target.mincustomer.value || !event.target.maxcustomer.value || !event.target.avgcookie.value){
+    return alert('All fields must be filled in.');
+  }
+  //create variables for new store
+  var newStoreName = event.target.storename.value;
+  var newStoreMinCustomer = parseInt(event.target.mincustomer.value);
+  var newStoreMaxCustomer = parseInt(event.target.maxcustomer.value);
+  var newStoreAvgCookie = parseInt(event.target.avgcookie.value);
+
+  new MakeShopLocation(newStoreName, newStoreMinCustomer, newStoreMaxCustomer, newStoreAvgCookie);
+
+  clearFields(event);
+
+  renderLocationRows();
+}
+
+storeForm.addEventListener('submit', handleAdditionalStore);
